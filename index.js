2b693014e652c3fe99e8a1c7d1cdb6c6302666dc
ZhangRGK <stamen0630@gmail.com>
2020-01-10 13:56:28 +0800

    Index page

---
+++ index.js
@@ -0,0 +1,24 @@
+function fn() {
+  console.log("ready");
+  if (window.tns) {
+    window.tns({
+      container: ".slider",
+      items: 1,
+      mode: "gallery",
+      autoPlay: true,
+      autoplayTimeout: 3500,
+      swipeAngle: false,
+      controls: false,
+      speed: 500,
+      nav: true,
+      navContainer: ".nav",
+      animateIn: "fade-in",
+      animateOut: "fade-out"
+    });
+  }
+}
+if (document.readyState != "loading") {
+  fn();
+} else {
+  document.addEventListener("DOMContentLoaded", fn);
+}
